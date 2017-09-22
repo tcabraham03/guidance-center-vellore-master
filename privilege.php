79cb7bc533aab14df74372e9ead2c5cacc27bf8d
Toni Christo Abraham <tonichristoabraham@gmail.com>
2017-09-22 16:44:24 +0530

    added php

---
+++ privilege.php
@@ -0,0 +1,29 @@
+<?php
+$id=$_GET['id'];
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "privilege_card_members";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM members where regno=$id";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "Registration no.: " . $row["regno"]. " - Name: " . $row["details"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+?>
