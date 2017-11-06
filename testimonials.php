1c8e777f8f3de03d3b1bfd53b41bb9689c7a3c6b
Toni Christo Abraham <tonichristoabraham@gmail.com>
2017-11-06 17:17:21 +0530

    Footer: Errors rectified

--- testimonials.php
+++ testimonials.php
@@ -9,6 +9,10 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="bower_components/materialize/dist/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="styles/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="bower_components/slick-carousel/slick/slick.css" rel="stylesheet">
+  <link rel="stylesheet" href="bower_components/slick-carousel/slick/slick-theme.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.css">
+  
 </head>
 <body>
   <nav class="soft-crust" role="navigation">
@@ -63,8 +67,8 @@
 
           $servername = "localhost";
           $username = "root";
-          $password = "pwd4MySQL";
-          $dbname = "privilege_card_members";
+          $password = "";
+          $dbname = "velloremtcguidance";
           // Create connection
           $conn = new mysqli($servername, $username, $password, $dbname);
           // Check connection
@@ -141,48 +145,49 @@
 
 
         <footer class="liz-orange page-footer ">
-    <div class="container">
-      <div class="row">
-        <div class="col l6 s12">
-          <h5 class="white-text"><img src="images/icon_destination.png" width="30" height="30"> Address</h5>
-          <h6 class="white-text">Mar Thoma New Guidance Home,<br/>Old Bypass Rd, Behind New Fish Market,<br/>Thottapalayam, Vellore Fort P.O,<br/>Vellore, Tamil Nadu 632-004<br/>India<br/><br/></h6>
-          <ul>
-           <li>
-             <div class="icon white-text">
-               <i class="fa fa-phone black-text" style="font-size:24px; width:28px; text-align:center;"></i> 0416 - 2220542
-             </div>
-           </li>
-           <li>
-             <div class="icon white-text">
-               <i class="fa fa-mobile black-text" style="font-size:30px; width:28px; text-align:center;"></i> +919445410542
-             </div>
-           </li>
-           <li>
-             <div class="icon white-text">
-               <i class="fa fa-envelope black-text" style="font-size:24px; width:28px; text-align:center;"></i> mtguidancehome@gmail.com
-             </div>
-           </li>
-          </ul>
-          <p class="grey-text text-lighten-4"></p>
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text"><img src="images/icon_destination.png" width="30" height="30"> Address</h5>
+              <h6 class="white-text">Mar Thoma New Guidance Home,<br/>Old Bypass Rd, Behind New Fish Market,<br/>Thottapalayam, Vellore Fort P.O,<br/>Vellore, Tamil Nadu 632-004<br/>India<br/><br/></h6>
+               <ul>
+                <li>
+                  <div class="icon white-text">
+                    <i class="fa fa-phone black-text" style="font-size:20px; width:28px; text-align:center;"></i> 0416 - 2220542
+                  </div>
+                </li>
+                <li>
+                  <div class="icon white-text">
+                    <i class="fa fa-mobile black-text" style="font-size:20px; width:28px; text-align:center;"></i> +919445410542
+                  </div>
+                </li>
+                <li>
+                  <div class="icon white-text">
+                    <i class="fa fa-envelope black-text" style="font-size:20px; width:28px; text-align:center;"></i> mtguidancehome@gmail.com
+                  </div>
+                </li>
+               </ul>
+              <p class="grey-text text-lighten-4"></p>
+            </div>
+            <div class="col l3 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li><a class="white-text" href="sitemap.html">Site Map</a></li>
+                <li><a class="white-text" href="https://www.facebook.com/Dr-Alexander-Mar-Thoma-Memorial-Fellowship-and-Guidance-Home-345380575539622/">Facebook Page</a></li>
+                <li><a class="white-text" href="login.html">Admin_Login</a></li>
+              </ul>
+            </div>
+            <div class="col l3 s12">
+              <h5 class="white-text">Support Us</h5>
+            </div>
+          </div>
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Links</h5>
-          <ul>
-            <li><a class="white-text" href="sitemap.html">Site Map</a></li>
-            <li><a class="white-text" href="https://www.facebook.com/Dr-Alexander-Mar-Thoma-Memorial-Fellowship-and-Guidance-Home-345380575539622/">Facebook Page</a></li>
-          </ul>
+        <div class="footer-copyright">
+          <div class="container">
+          Made by <a class="orange-text text-lighten-1" href="http://www.mtguidancehomevellore.in">© 2017 MTC Guidance Home, Vellore. </a> All Rights Reserved®
+          </div>
         </div>
-        <div class="col l3 s12">
-          <h5 class="white-text">Support Us</h5>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-      Made by <a class="orange-text text-lighten-1" href="http://www.mtguidancehomevellore.in">© 2017 MTC Guidance Home, Vellore. </a> All Rights Reserved®
-      </div>
-    </div>
-  </footer>
+      </footer>
 
 </body>
 </html>
